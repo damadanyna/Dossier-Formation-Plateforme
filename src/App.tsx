0704b5fd36ab13a9b2334afcaed972f4f2c96017
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2023-02-01 20:47:38 +0300

    chore: add notistack for toast notifications 🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import AuthContextProvider from './components/contexts/AuthContextProvider';
 import AppRoutes from './routes';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppRoutes />
+      <SnackbarProvider>
+        <AuthContextProvider>
+          <CssBaseline />
+          <AppRoutes />
+        </AuthContextProvider>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
