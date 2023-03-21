5d0291f73f0aa52b3efee47a84389ad5499b9022
Tamby Mampionona <therihar@etceterum.net>
2023-03-21 22:19:26 +0300

    Configuration du fichier json pour les textes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-
+import textes from './data/textes.json';
 function App() {
   return (
+    <div>
+    <h1>{textes.titre}</h1>
+    <p>{textes.description}</p>
     <h1 className="text-3xl font-bold underline"> Mety ilay izy </h1>
+    <button>{textes.bouton}</button>
+  </div>
   );
 }
 
