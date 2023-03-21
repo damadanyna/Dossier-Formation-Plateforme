023b6c9b36a15283e4c19b74f09c2e8547a06773
angeloralaivelo <81219758+angelorlv@users.noreply.github.com>
2023-03-21 21:37:35 +0100

    Configuration tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import './index.css';
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
