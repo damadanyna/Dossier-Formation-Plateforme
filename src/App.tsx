97eea55fda04a1e7eabb967857e35d54d20a858a
dama <damadanyna@gmail.com>
2023-03-23 18:44:15 +0300

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+ 
 import PublicLayout from './layouts/main';
 import Home from './pages/home';
-import Contact from './pages/Contact'
+import Contact from './pages/Contact';
 import FormaulairePaiement from './pages/formulairePayement';
+import Tarif from './pages/Tarif';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() { 
@@ -12,10 +14,11 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/formulairePaiement' element={<FormaulairePaiement />} />
+          <Route path='/tarif' element={<Tarif />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
